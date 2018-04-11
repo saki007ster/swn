a4192654ae2d77c846118166b7f802ecf3051c6c
Saket Kumar <saki007ster@gmail.com>
2018-04-12 01:44:36 +0530

    Fixing some content.

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 /**
- * menu.js
+ * menu2.js
  * http://www.codrops.com
  *
  * Licensed under the MIT license.
@@ -14,6 +14,8 @@
 			this.DOM = {el: el};
 			this.DOM.bg = this.DOM.el.querySelector('.grim__item-bg');
 			this.DOM.inner = this.DOM.el.querySelector('.grim__item-content > .grim__item-inner');
+			this.DOM.img = this.DOM.el.querySelector('.grim__item-img');
+			this.DOM.cover = this.DOM.el.querySelector('.grim__item-bg-cover');
 			this.DOM.link = this.DOM.el.querySelector('a.grim__link');
 			this.pos = pos;
 		}
@@ -24,7 +26,7 @@
 				anime.remove(this.DOM.bg);
 				anime({
 					targets: this.DOM.bg,
-					duration: this.DOM.bg.dataset.duration || 30+this.pos*20, // increment as more boxes get revealed..
+					duration: this.DOM.bg.dataset.duration || 40+this.pos*20, // increment as more boxes get revealed..
 					easing: this.DOM.bg.dataset.easing || 'easeInOutQuad',
 					opacity: {
 						value: 1,
@@ -32,52 +34,74 @@
 					},
 					scaleY: this.pos % 2 === 0 ? [0,1] : 1,
 					scaleX: Math.abs(this.pos % 2) == 1 ? [0,1] : 1,
-					complete: resolve
-				});
+					complete: () => {
+						if ( this.DOM.img && this.DOM.cover ) {
+							this.DOM.img.style.opacity = 1;
+							this.DOM.cover.style.opacity = 1;
+							this.DOM.cover.style.transformOrigin = '100% 50%';
+							anime.remove(this.DOM.cover);
+							anime({
+								targets: this.DOM.cover,
+								duration: 700,
+								easing: 'easeOutQuint',
+								scaleX: [1,0]
+							});
+						}
 
-				if ( this.DOM.inner ) {
-					anime.remove(this.DOM.inner);
-					anime({
-						targets: this.DOM.inner,
-						duration: 1300,
-						delay: 550,
-						easing: 'easeOutExpo',
-						opacity: {
-							value: 1,
-							duration: 1
-						},
-						translateY: ['100%','0%']
-					});
-				}
+						if ( this.DOM.inner ) {
+							anime.remove(this.DOM.inner);
+							anime({
+								targets: this.DOM.inner,
+								duration: 1200,
+								delay: 150,
+								easing: 'easeOutQuint',
+								opacity: {
+									value: 1,
+									duration: 1
+								},
+								translateY: ['100%','0%']
+							});
+						}
+						
+						resolve();
+					}
+				});
 			});
 		}
 		close() {
 			return new Promise((resolve, reject) => {
-				this.DOM.bg.style.transformOrigin = this.pos % 2 === 0 ? '50% 0%' : '100% 50%';
+				const animateBoxFn = () => {
+					this.DOM.bg.style.transformOrigin = this.pos % 2 === 0 ? '50% 0%' : '100% 50%';
 
-				anime.remove(this.DOM.bg);
-				anime({
-					targets: this.DOM.bg,
-					duration: this.DOM.bg.dataset.duration || 10+this.pos*10, // increment as more boxes get revealed..
-					easing: this.DOM.bg.dataset.easing || 'easeInOutQuad',
-					opacity: {
-						value: 0,
-						delay: this.DOM.bg.dataset.duration || 10+this.pos*10,
-						duration: 1
-					},
-					scaleY: this.pos % 2 === 0 ? [1,0] : 1,
-					scaleX: Math.abs(this.pos % 2) == 1 ? [1,0] : 1,
-					complete: resolve
-				});
-
-				if ( this.DOM.inner ) {
-					anime.remove(this.DOM.inner);
+					anime.remove(this.DOM.bg);
 					anime({
-						targets: this.DOM.inner,
-						duration: 100,
+						targets: this.DOM.bg,
+						duration: this.DOM.bg.dataset.duration || 80+this.pos*10,
+						easing: this.DOM.bg.dataset.easing || 'easeInOutQuad',
+						opacity: {
+							value: 0,
+							delay: this.DOM.bg.dataset.duration || 40+this.pos*10,
+							duration: 1
+						},
+						scaleY: this.pos % 2 === 0 ? [1,0] : 1,
+						scaleX: Math.abs(this.pos % 2) == 1 ? [1,0] : 1,
+						complete: resolve
+					});
+				};
+
+				if ( this.DOM.img && this.DOM.cover ) {
+					anime.remove(this.DOM.img);
+					anime({
+						targets: this.DOM.img,
+						duration: 200,
 						easing: 'linear',
 						opacity: 0
 					});
+				}
+				animateBoxFn();
+
+				if ( this.DOM.inner ) {
+					this.DOM.inner.style.opacity = 0;
 				}
 			});
 		}
@@ -143,13 +167,18 @@
 	}
 
 	let DOM = {};
-	DOM.grim = document.querySelector('.grim');
-	DOM.menu = new Menu(DOM.grim);
-	DOM.menuCtrls = {
-		open: document.querySelector('.menu-trigger'),
-		close: document.querySelector('.menu-trigger--close')
-	};
+
+	imagesLoaded(document.querySelectorAll('.grim__item-img'), {background: true}, () => {
+		document.body.classList.remove('loading');
+
+		DOM.grim = document.querySelector('.grim');
+		DOM.menu = new Menu(DOM.grim);
+		DOM.menuCtrls = {
+			open: document.querySelector('.menu-trigger'),
+			close: document.querySelector('.menu-trigger--close')
+		};
 	
-	DOM.menuCtrls.open.addEventListener('click', () => DOM.menu.open());
-	DOM.menuCtrls.close.addEventListener('click', () => DOM.menu.close());
+		DOM.menuCtrls.open.addEventListener('click', () => DOM.menu.open());
+		DOM.menuCtrls.close.addEventListener('click', () => DOM.menu.close());
+	});
 }
